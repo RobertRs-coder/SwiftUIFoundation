868c5b5d6998ea833bdc76f999205b73d5a02dac
Roberto Rojo <rrojo.va@gmail.com>
2022-11-01 03:50:31 +0100

    Initial Commit

---
+++ SwiftUIFoundation/SwiftUIFoundationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFoundationApp.swift
+//  SwiftUIFoundation
+//
+//  Created by Roberto Rojo Sahuquillo on 1/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFoundationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
